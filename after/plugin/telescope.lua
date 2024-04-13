f437b2f0b6d8b41fb00a310315ac2c3616b89ebe
maundjs <maundjs@gmail.com>
2024-04-13 18:08:01 +0100

    Telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,7 @@
 local builtin = require('telescope.builtin')
+-- Telescope fuzzy finder key bindings
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fp', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ")})
+end)
